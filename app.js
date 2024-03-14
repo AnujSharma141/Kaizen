bfdca2615d27259d444e957e6cf8b5d85aaf6989
anujsharma141 <19ucc109@lnmiit.ac.in>
2024-03-14 12:45:23 +0530

    update: landing page ui!

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
-import cron from 'node-cron'
-
-import ping from './src/utils/ping'
 
 import typeDefs from './graphql/schema'
 import resolvers from './graphql/resolver'
-
-// cron: self pinging service
-cron.schedule('*/5 * * * * ', () => {
-  ping.handler('https://kaizen-api-ju41.onrender.com/')
-})
 
 const app = express()
 
